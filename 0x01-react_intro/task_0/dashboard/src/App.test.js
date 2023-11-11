73413e4d487b62659ad71da14a6ab471eccac1a4
astro175 <aspark400@gmail.com>
2023-11-11 11:22:44 +0100

    Used Enzyme utility for testing

--- 0x01-react_intro/task_0/dashboard/src/App.test.js
+++ 0x01-react_intro/task_0/dashboard/src/App.test.js
@@ -1,35 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 import Notifications from './Notifications';
 
 describe('Test for App component', () => {
     it('Checks for App-header div', () => {
-        render(<App/>)
-        const AppHeader = screen.getByTestId('App-header');
-        expect(AppHeader).toBeInTheDocument()
+        const wrapper = shallow(<App />)
+        const AppHeader = wrapper.find('.App-header')
+        expect(AppHeader.exists()).toBe(true)
     });
     it('Checks for App-footer div', () => {
-        render(<App/>)
-        const Appfooter = screen.getByTestId('App-footer');
-        expect(Appfooter).toBeInTheDocument()
+        const wrapper = shallow(<App />)
+        const Appfooter = wrapper.find('.App-footer');
+        expect(Appfooter.exists()).toBe(true)
     });
     it('Checks for App-body div', () => {
-        render(<App/>);
-        const AppBody = screen.getByTestId('App-body');
-        expect(AppBody).toBeInTheDocument()
+        const wrapper = shallow(<App />)
+        const AppBody = wrapper.find('.App-body');
+        expect(AppBody.exists()).toBe(true)
     });
 });
 
 describe('Checks for Notification Component', () => {
     it('Checks if three li elements are rendered', () => {
-        render(<Notifications />);
-        const liItems = screen.getAllByRole('listitem');
+        const wrapper = shallow(<Notifications />)
+        const liItems = wrapper.find('li');
         expect(liItems).toHaveLength(3);
     });
 
     it('Checks for paragraph text is rendered', () => {
-        render(<Notifications />);
-        const text = screen.getByText('Here is the list of notifications');
-        expect(text).toBeInTheDocument();
+        const wrapper = shallow(<Notifications />)
+        const text = wrapper.contains('Here is the list of notifications')
+        expect(text).toBe(true);
     })
 })
